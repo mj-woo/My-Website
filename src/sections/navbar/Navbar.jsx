b6dff70a5637fc55cd5fd6a9b6a6b8c56bf34852
Minji Woo <minjiwoo03@gmail.com>
2023-09-18 14:05:42 -0400

    nav

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -14,10 +14,9 @@ const Navbar = ({closeModal}) => {
   return (
     <nav>
       <div className='container nav__container'>
-        {/* <a href="/" className='nav__logo' onClick={handleReload}>
+        <a href="/" className='nav__logo' onClick={handleReload}>
           <h1>MJW</h1>
-        </a> */}
-        <h1><Link to={'/'} className='nav__logo' onClick={handleReload}>MJW</Link></h1>
+        </a>
         <ul className='nav__menu'>
           {
             data.map(item => <li key={item.id}><Link to={item.link} onClick={handleReload}>{item.title}</Link></li>)
